de9cbbcf3fb783ee5a2f149d5f73e879a62d6213
Vlad <vlad_chekmarev_01@mail.ru>
2021-01-21 17:09:23 +0300

    add RoadMapList

--- Hacaton54/Controllers/RoadMapController.cs
+++ Hacaton54/Controllers/RoadMapController.cs
@@ -4,6 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hacaton54.Models.DataModel;
+using Hacaton54.BackEnd.ExcelHelp;
+using Hacaton54.Models.Repositories;
+using Microsoft.AspNetCore.Http;
 
 namespace Hacaton54.Controllers
 {
@@ -11,9 +15,39 @@ namespace Hacaton54.Controllers
     [Authorize]
     public class RoadMapController : Controller
     {
+
+        private ExcelHelper excelHelper = new ExcelHelper();       
+
+        private GroupRepository groupRepository;
+
+        private RoadMapRepository roadMapRepository;
+     
+        public RoadMapController(ks54AISContext _context)
+        {
+            //this.context = _context; 
+            roadMapRepository = new RoadMapRepository(_context);
+            groupRepository = new GroupRepository(_context);
+        }
+
         public IActionResult CommonRoadMap()
         {
-            return View();
+            List<RoadMap> roadMaps = roadMapRepository.GetRoadMaps(); 
+            return View(roadMaps);
+        }
+
+        [HttpPost]
+        public IActionResult CommonRoadMap(string search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+            {
+
+            }
+            return View(RoadMapRepository.RoadMaps);
+        }
+     
+        public IActionResult ExportExcel()
+        {
+            return View(); 
         }
 
         public IActionResult AddRoadMap()
--- Hacaton54/Models/Repositories/RoadMapRepository.cs
+++ Hacaton54/Models/Repositories/RoadMapRepository.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hacaton54.Models.DataModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hacaton54.Models.Repositories
 {
     public class RoadMapRepository : ModelRepository
     {
+        private ks54AISContext context;
+
+        public static List<RoadMap> RoadMaps { get; private set; }
+
+        public RoadMapRepository(ks54AISContext _context)
+        {
+            this.context = _context; 
+        }
+
+        public List<RoadMap> GetRoadMaps()
+        {
+            RoadMaps = context.RoadMaps.Include(p => p.Group.Department)
+                .Include(p => p.Group)
+                .Include(p => p.Group.Department.HeadOfDepartmentNavigation)
+                .Include(p => p.Group.GroupSpeciality)
+                .Include(p => p.Attestation)
+                .Include(p => p.Attestation.Month)
+                .Include(p => p.Attestation.DisciplineEmployee.Employee)
+                .Include(p => p.Attestation.DisciplineEmployee.Discipline)
+                .ToList();
+            return RoadMaps; 
+        }
+
+        
+
     }
 }
